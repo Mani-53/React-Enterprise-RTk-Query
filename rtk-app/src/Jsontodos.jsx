c7dbc9b1a9df51abc5e7558260270f5d3ffc874a
Mani <manitejapalyam@gmail.com>
2026-03-30 15:12:15 +0530

    hi

--- rtk-app/src/Jsontodos.jsx
+++ rtk-app/src/Jsontodos.jsx
@@ -9,25 +9,27 @@ function Jsontodos() {
     // console.log(data)
     return (
         <div className="json">
-            <div id="jsoninput">
-                <input type="text" onChange={(e) => { setTodo(e.target.value) }} />
-                <button onClick={() => { add(todo) }}>ADD</button>
+            <div id="secondjson"> 
+                <div id="jsoninput">
+                    <input type="text" onChange={(e) => { setTodo(e.target.value) }} />
+                    <button onClick={() => { add(todo) }}>ADD</button>
+                </div>
+                {
+                    isLoading && <img src="https://cdnl.iconscout.com/lottie/premium/thumb/loading-circle-7008030-5740009.gif" alt="" className="loadingimage" />
+                }
+                {
+                    data && (
+                        <ul id="jsonul">
+                            {
+                                data.map((t, i) => {
+                                    return <li key={i}>{t.title}
+                                        <button onClick={() => { del(t.id) }}>Delete</button></li>
+                                })
+                            }
+                        </ul>
+                    )
+                }
             </div>
-            {
-                isLoading && <img src="https://cdnl.iconscout.com/lottie/premium/thumb/loading-circle-7008030-5740009.gif" alt="" className="loadingimage"/>
-            }
-            {
-                data && (
-                    <ul id="jsonul">
-                        {
-                            data.map((t, i) => {
-                                return <li key={i}>{t.title}
-                                    <button onClick={() => { del(t.id) }}>Delete</button></li>
-                            })
-                        }
-                    </ul>
-                )
-            }
         </div>
     )
 }
